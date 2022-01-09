5400fe95d00daec2fbeb07d2df244fbb1ea25a4d
Ashish <97358355+ashish-android@users.noreply.github.com>
2022-01-08 18:10:57 -0600

    test

--- app/src/main/java/com/example/myapplication/FirstFragment.kt
+++ app/src/main/java/com/example/myapplication/FirstFragment.kt
@@ -18,8 +18,6 @@ class FirstFragment : Fragment() {
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
-
-
     
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
